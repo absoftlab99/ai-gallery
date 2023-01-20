745354c63380c03ca25bf70209ba5f931df2b7fe
mdsahhinreza <mdsahhinreza@gmail.com>
2023-01-20 22:17:26 +0600

    Add Loader

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { BsBroadcastPin, BsBookmark, BsFillGridFill, BsPlusLg } from "react-icons/bs";
+import {
+  BsBroadcastPin,
+  BsBookmark,
+  BsFillGridFill,
+  BsPlusLg,
+} from "react-icons/bs";
 import { GiBrain } from "react-icons/gi";
 import { FcGoogle, FcRedo } from "react-icons/fc";
 import { AuthContext } from "../../Context/UserContext";
@@ -8,90 +13,103 @@ import { useContext } from "react";
 import { GoogleAuthProvider } from "firebase/auth";
 
 const Header = () => {
+  const { googlePopUp, user, logOut, loading } = useContext(AuthContext);
 
-    const { googlePopUp, user, logOut, loading } = useContext(AuthContext)
+  const provider = new GoogleAuthProvider();
 
-    const provider = new GoogleAuthProvider();
+  const googleSingIn = (event) => {
+    event.preventDefault();
+    googlePopUp(provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-    const googleSingIn = (event) => {
-        event.preventDefault();
-        googlePopUp(provider)
-            .then((result) => {
-                const user = result.user;
-                console.log(user);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    };
+  const signOutHandler = () => {
+    logOut();
+  };
 
-    const signOutHandler = () => {
-        logOut();
-    }
-
-    const menu = (
+  const menu = (
+    <>
+      <li>
+        <Link to={"/"}>
+          <BsBroadcastPin className=""></BsBroadcastPin>Discover
+        </Link>
+      </li>
+      {user ? (
         <>
-            <li>
-                <Link to={"/"}><BsBroadcastPin className=""></BsBroadcastPin>Discover</Link>
-            </li>
-            {
-                user ?
-                    <>
-                        <li>
-                            <Link to={"/bookmark"}><BsBookmark></BsBookmark>Bookmarks</Link>
-                        </li>
-                        <li>
-                            <Link to={"/myGallery"}><BsFillGridFill></BsFillGridFill>My Gallery</Link>
-                        </li>
-                        <li>
-                            <Link to={"/addAi"}><BsPlusLg></BsPlusLg>Add New Ai Tool</Link>
-                        </li>
-                    </>
-                    : ""
-            }
+          <li>
+            <Link to={"/bookmark"}>
+              <BsBookmark></BsBookmark>Bookmarks
+            </Link>
+          </li>
+          <li>
+            <Link to={"/myGallery"}>
+              <BsFillGridFill></BsFillGridFill>My Gallery
+            </Link>
+          </li>
+          <li>
+            <Link to={"/addAi"}>
+              <BsPlusLg></BsPlusLg>Add New Ai Tool
+            </Link>
+          </li>
         </>
-    );
-    return (
-        <div className="navbar bg-base-100">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M4 6h16M4 12h8m-8 6h16"
-                            />
-                        </svg>
-                    </label>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-                    >
-                        {menu}
-                    </ul>
-                </div>
-                <button className="btn btn-ghost normal-case text-xl">
-                    <GiBrain className="mr-2 text-4xl"></GiBrain>Ai Gallery
-                </button>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">{menu}</ul>
-            </div>
-            <div className="navbar-end">
-                {
-                    user ? <button onClick={signOutHandler} className="btn"><FcRedo className="text-2xl mr-2"></FcRedo> Logout</button> : <button onClick={googleSingIn} className="btn"><FcGoogle className="text-2xl mr-2"></FcGoogle> Sign In</button>
-                }
-            </div>
+      ) : (
+        ""
+      )}
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {menu}
+          </ul>
         </div>
-    );
+        <button className="btn btn-ghost normal-case text-xl">
+          <GiBrain className="mr-2 text-4xl"></GiBrain>Ai Gallery
+        </button>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{menu}</ul>
+      </div>
+      <div className="navbar-end">
+        {user ? (
+          <button onClick={signOutHandler} className="btn">
+            <FcRedo className="text-2xl mr-2"></FcRedo> Logout
+          </button>
+        ) : (
+          <button onClick={googleSingIn} className="btn">
+            <FcGoogle className="text-2xl mr-2"></FcGoogle> Sign In
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/Components/Layout/Main.js
+++ src/Components/Layout/Main.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Outlet } from "react-router-dom";
+import { AuthContext } from "../../Context/UserContext";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 
 const Main = () => {
+  const { loading } = useContext(AuthContext);
   return (
     <>
-      <Header></Header>
-      <div className="outlet">
-        <Outlet></Outlet>
-      </div>
-      <Footer></Footer>
+      {loading ? (
+        <div className="hero min-h-screen bg-base-200">
+          <div className="hero-content text-center">
+            <div className="max-w-md">
+              <progress className="progress w-56"></progress>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <>
+          <Header></Header>
+          <div className="outlet">
+            <Outlet></Outlet>
+          </div>
+          <Footer></Footer>
+        </>
+      )}
     </>
   );
 };
