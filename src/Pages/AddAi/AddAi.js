f66e8081d7cadbf7fb98bc4ce47b3bdeabf670bf
Abdullah Al Mahmud <58851539+absoftlab99@users.noreply.github.com>
2023-01-22 11:51:35 +0600

    all responsive problem are solved

--- src/Pages/AddAi/AddAi.js
+++ src/Pages/AddAi/AddAi.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import { IoMdPaperPlane } from "react-icons/io";
-import Multiselect from 'multiselect-react-dropdown';
 
 
 const AddAi = () => {
     return (
-        <div className="hero min-h-screen w-100" style={{ backgroundImage: `url("https://i.ibb.co/8YYjxp5/istock-1200937823.jpg")` }}>
+        <div className="hero min-h-screen w-full" style={{ backgroundImage: `url("https://i.ibb.co/8YYjxp5/istock-1200937823.jpg")` }}>
             <div className="hero-overlay"></div>
-            <div className="hero">
-                <div className="">
-                    <div className="card shadow-2xl w-100 bg-base-100 bg-opacity-90">
+            <div className="hero-content">
+                <div className="hero-content">
+                    <div className="card shadow-2xl w-100 bg-base-300 bg-opacity-90">
                         <div className="card-body">
                             <h1 className='text-4xl mb-3'>Submit a Ai Tool</h1>
-                            <div className='flex gap-4'>
+                            <div className='md:flex gap-4'>
                                 <div className="form-control w-100">
                                     <input type="text" placeholder="Tool Name" className="input input-bordered" />
                                 </div>
-                                <div className="form-control">
+                                <div className="form-control mt-4 md:mt-0">
                                     <input type="text" placeholder="Web Link" className="input input-bordered" />
                                 </div>
                             </div>
-                            <div className="form-control">
+                            <div className="form-control mt-4">
                                 <textarea className='textarea textarea-bordered' placeholder='Tool Description'></textarea>
                             </div>
                             <div className="form-control mt-6">
